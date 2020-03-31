c03480bdf758c6d188bc5f8945ba3962110dc3a7
abc <abc@example.com>
2020-03-30 21:30:40 -0400

    shop page, preview component and item component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,15 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import './App.css';
-import{ HomePage } from './pages/homepage.component.jsx'
-
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-);
+import{ HomePage } from './pages/homepage/homepage.component.jsx'
+import ShopPage from './pages/shop/shop.component';
 
 function App() {
   return (
     <div className="homepage">
       <Switch>
        <Route exact path='/' component={HomePage}/>
-      <Route  path='/shop/hats' component={HatsPage}/>
+      <Route  path='/shop' component={ShopPage}/>
      </Switch>
     </div>
   );
